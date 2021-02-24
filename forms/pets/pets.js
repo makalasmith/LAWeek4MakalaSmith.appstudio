6c6fa0f24d9e01dda7cdc98a629f3d5284afd512
Makala Smith <mms96354@creighton.edu>
2021-02-24 14:25:29 -0600

    adding sal and third pet na,e

--- forms/pets/pets.js
+++ forms/pets/pets.js
@@ -1,2 +1,9 @@
 let petNames = ["Darcy", "Jack", "Jerry", "Marty", "Vito", "Vinny", "Riley", "Saddie", "Marge", "Sam"]
 console.log(petNames)
+
+// let withSal = petNames
+
+petNames.push("Sal")
+console.log(petNames)
+
+console.log(`The third pet is named ${petNames[2]}.`)
